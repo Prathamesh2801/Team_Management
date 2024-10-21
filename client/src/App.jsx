cf0caae82d83174e0e769995c4a656577f432ff6
Prathamesh2801 <truepratham2801@gmail.com>
2024-10-21 19:05:14 +0530

    Added Clerk Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-export const App = () => {
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+export default function App() {
   return (
-    <>
-      <h1 className="text-emerald-700 mx-auto  text-4xl">commit from adi</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsa
-        facilis ipsam excepturi, dolore magnam sit suscipit alias incidunt animi
-        eius magni, tenetur quod quidem hic ex voluptatibus. Ullam similique
-        quaerat repellat illo nam quis ratione reiciendis aperiam labore esse!
-      </p>
-    </>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
-};
+}
