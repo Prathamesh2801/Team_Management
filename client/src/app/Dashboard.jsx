d3dc8285c0d94f6157a880dc0f3650f46a667e66
arxen <ar006985@gmail.com>
2024-11-01 20:37:11 +0530

    modified sidebar & updated dashboard

--- client/src/app/Dashboard.jsx
+++ client/src/app/Dashboard.jsx
@@ -5,20 +5,17 @@ import { QuickActions } from "../components/dashboard/QuickActions";
 
 export const Dashboard = () => {
   return (
-    <div className="bg-neutral-950">
+    <div className="bg-white dark:bg-neutral-950">
       <Layout>
         <div className="grid grid-cols-1 gap-6 p-6 lg:grid-cols-3">
-          {/* Quick Actions Section */}
           <div className="lg:col-span-3">
             <QuickActions />
           </div>
-          
-          {/* Team Projects Section */}
+
           <div className="lg:col-span-2">
             <TeamProjects />
           </div>
-          
-          {/* Task Activity Section */}
+
           <div className="lg:col-span-1">
             <TaskActivity />
           </div>
--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -4,9 +4,7 @@ export const Layout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 bg-white dark:bg-neutral-950">
-        {children}
-      </main>
+      <main className="flex-1 bg-white dark:bg-neutral-950">{children}</main>
     </div>
   );
 };
